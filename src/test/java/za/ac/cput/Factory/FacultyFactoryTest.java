74c9bf9cee2b419e26a25025010dd6a7bf00f73d
Caven Fernanda <fernanda17caven@gmail.com>
2021-06-11 19:53:20 +0200

    Test cases of UniversityFactoryTest and FacultyFactoryTest

--- src/test/java/za/ac/cput/Factory/FacultyFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/FacultyFactoryTest.java
@@ -1,7 +1,40 @@
 package za.ac.cput.Factory;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import za.ac.cput.Entity.Faculty;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FacultyFactoryTest {
 
+    Faculty faculty = FacultyFactory.buildFaculty("Informatics and Design","0219596767");
+    Faculty faculty1 = FacultyFactory.buildFaculty("Informatics and Design","0219596767");
+
+
+    @Test
+    public void ObjectEquality(){
+        assertNotEquals(faculty,faculty1);
+    }
+
+    @Test
+    public void ObjectIdentity(){
+
+        assertNotEquals(faculty,faculty1);
+    }
+
+    @Test
+    @Timeout(2)
+    public void TimeoutTest()
+    {
+        ObjectIdentity();
+    }
+
+    @Disabled
+    @Test
+    public void ObjectEquals()
+    {
+        assertNotEquals(faculty,faculty1);
+    }
 }
--- src/test/java/za/ac/cput/Factory/UniversityFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/UniversityFactoryTest.java
@@ -1,7 +1,39 @@
 package za.ac.cput.Factory;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import za.ac.cput.Entity.University;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UniversityFactoryTest {
 
+    University university = UniversityFactory.buildUniversity("Cape Peninsula University of Technology", "Cape Town","Hanover St, Zonnebloem, Cape Town, 7925");
+    University university1 = UniversityFactory.buildUniversity("Cape Peninsula University of Technology", "Cape Town","Hanover St, Zonnebloem, Cape Town, 7925");
+
+    @Test
+    public void ObjectEquality(){
+        assertNotEquals(university,university1);
+    }
+
+    @Test
+    public void ObjectIdentity(){
+
+        assertNotEquals(university,university1);
+    }
+
+    @Test
+    @Timeout(2)
+    public void TimeoutTest()
+    {
+        ObjectIdentity();
+    }
+
+    @Disabled
+    @Test
+    public void ObjectEquals()
+    {
+        assertNotEquals(university,university1);
+    }
 }
