2eb71ef451ad2506b2d6cf8e92e0da3f29f4fc12
Caven Fernanda <fernanda17caven@gmail.com>
2021-10-03 21:28:41 +0200

    Created Faculty and repository

--- src/main/java/za/ac/cput/Repository/IRepository.java
+++ src/main/java/za/ac/cput/Repository/IRepository.java
@@ -6,5 +6,5 @@ public interface IRepository<T, ID>{
     public T create(T t);
     public T read(ID id);
     public T update(T t);
-    public T delete(ID id);
+    public boolean delete(ID id);
 }
